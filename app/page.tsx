d248e04b24fbfae1d62db96eedef7f40b029acf2
FacundoCasalegnoCesano <facucasalegno@gmail.com>
2025-10-29 14:30:54 -0300

    minor changes

--- app/page.tsx
+++ app/page.tsx
@@ -15,12 +15,12 @@ export default function HomePage() {
       <div>
         <ProductsSection
           title="PRODUCTOS MAS VENDIDOS"
-          productIds={[60002, 90001, 180002, 60001, 30001]}
+          productIds={[60002, 90001, 180002, 60001, 30001,360009,360012,270001]}
         />
       </div>
       <BrandsSection />
       <NavigationSection />
-      <ProductsSection title="PRODUCTOS RECOMENDADOS" productIds={[180001, 150002, 30002]} />
+      <ProductsSection title="PRODUCTOS RECOMENDADOS" productIds={[180001, 150002, 30002,360005,360007,360002,360010]} />
       <EcoFriendlySection />
       <FooterSection />
     </PageLayout>
